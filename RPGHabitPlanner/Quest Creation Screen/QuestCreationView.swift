d24a4d9d6df4557bee3a0ca3a9befce6047b9a94
makisacik <glsabaton@outlook.com>
2024-11-04 23:52:49 +0000

    Embed view in scroll view and avoid keyboard to push views up

--- RPGHabitPlanner/Quest Creation Screen/QuestCreationView.swift
+++ RPGHabitPlanner/Quest Creation Screen/QuestCreationView.swift
@@ -25,90 +25,93 @@ struct QuestCreationView: View {
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
-                TextField("Quest Title", text: $questTitle)
-                    .autocorrectionDisabled()
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .shadow(radius: 3)
-                
-                TextField("Quest Description", text: $questDescription)
-                    .autocorrectionDisabled()
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .shadow(radius: 3)
-                
-                DatePicker("Due Date", selection: $questDueDate, displayedComponents: [.date])
-                    .padding()
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .shadow(radius: 3)
-                
-                Toggle(isOn: $isMainQuest) {
-                    Text("Is this a main quest?")
-                        .font(.headline)
-                }
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .shadow(radius: 3)
-                
-                Toggle(isOn: $isRepetitiveQuest) {
-                    Text("Is this a repetitive quest?")
-                        .font(.headline)
-                }
-                .padding()
-                .background(Color(.systemGray6))
-                .cornerRadius(8)
-                .shadow(radius: 3)
-                
-                VStack {
-                    Text("Quest Difficulty")
-                        .font(.headline)
-                    
-                    StarRatingView(rating: $difficulty)
-                }
-                .padding()
-
-                Button(action: {
-                    viewModel.saveQuest(
-                        title: questTitle,
-                        isMainQuest: isMainQuest,
-                        info: questDescription,
-                        difficulty: difficulty,
-                        dueDate: questDueDate
-                    )
-                }) {
-                    Text("Save Quest")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
+            ScrollView {
+                VStack(spacing: 20) {
+                    TextField("Quest Title", text: $questTitle)
+                        .autocorrectionDisabled()
                         .padding()
-                        .background(Color.blue)
+                        .background(Color(.systemGray6))
                         .cornerRadius(8)
                         .shadow(radius: 3)
+                    
+                    TextField("Quest Description", text: $questDescription)
+                        .autocorrectionDisabled()
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .shadow(radius: 3)
+                    
+                    DatePicker("Due Date", selection: $questDueDate, displayedComponents: [.date])
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .shadow(radius: 3)
+                    
+                    Toggle(isOn: $isMainQuest) {
+                        Text("Is this a main quest?")
+                            .font(.headline)
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .shadow(radius: 3)
+                    
+                    Toggle(isOn: $isRepetitiveQuest) {
+                        Text("Is this a repetitive quest?")
+                            .font(.headline)
+                    }
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .shadow(radius: 3)
+                    
+                    VStack {
+                        Text("Quest Difficulty")
+                            .font(.headline)
+                        
+                        StarRatingView(rating: $difficulty)
+                    }
+                    .padding()
+
+                    Button(action: {
+                        viewModel.saveQuest(
+                            title: questTitle,
+                            isMainQuest: isMainQuest,
+                            info: questDescription,
+                            difficulty: difficulty,
+                            dueDate: questDueDate
+                        )
+                    }) {
+                        Text("Save Quest")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(8)
+                            .shadow(radius: 3)
+                    }
+                    .padding(.top, 20)
+                    .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Error"),
+                            message: Text(viewModel.errorMessage ?? "Unknown error"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                    .alert(isPresented: $showSuccessMessage) {
+                        Alert(
+                            title: Text("Success"),
+                            message: Text("Quest saved successfully!"),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
+                    
+                    Spacer()
                 }
-                .padding(.top, 20)
-                .alert(isPresented: $showAlert) {
-                    Alert(
-                        title: Text("Error"),
-                        message: Text(viewModel.errorMessage ?? "Unknown error"),
-                        dismissButton: .default(Text("OK"))
-                    )
-                }
-                .alert(isPresented: $showSuccessMessage) {
-                    Alert(
-                        title: Text("Success"),
-                        message: Text("Quest saved successfully!"),
-                        dismissButton: .default(Text("OK"))
-                    )
-                }
-                
-                Spacer()
+                .padding()
             }
-            .padding()
+            .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Create New Quest")
             .onTapGesture {
                 UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
@@ -131,6 +134,7 @@ struct QuestCreationView: View {
         isRepetitiveQuest = false
     }
 }
+
 
 #Preview {
     let questDataService = QuestCoreDataService()
